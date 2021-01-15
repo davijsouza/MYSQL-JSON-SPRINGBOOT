b09c0f67847473ce6e79c58bd8c2574b6d7f5867
davijsouza32 <davijsouza32@gmail.com>
2021-01-15 19:36:59 -0300

    commit inicial

---
+++ src/main/java/com/example/Book.java
@@ -0,0 +1,48 @@
+package com.example;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.Type;
+import org.hibernate.annotations.TypeDef;
+
+import com.vladmihalcea.hibernate.type.json.JsonStringType;
+
+@Entity(name = "Book")
+@Table(name = "book")
+@TypeDef(name = "json", typeClass = JsonStringType.class)
+public class Book {
+ 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO) 
+    private Long id;
+ 
+    @Type(type = "json")
+    @Column(columnDefinition = "json")
+    private Map<String, String> properties = new HashMap<>();
+   
+    public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Map<String, String> getProperties() {
+        return properties;
+    }
+ 
+    public Book setProperties(Map<String, String> properties) {
+        this.properties = properties;
+        return this;
+    }
+ 
+}
---
+++ src/main/java/com/example/BookController.java
@@ -0,0 +1,52 @@
+package com.example;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class BookController {
+
+	@Autowired
+	private BookRepository repository;
+
+	@GetMapping("/getData")
+	public Iterable<Book> getData() {
+		return repository.findAll();
+	}
+
+	@PostMapping("/addData")
+	@ResponseStatus(HttpStatus.CREATED)
+	public Book addData(@RequestBody Book tableData) {
+		return repository.save(tableData);
+	}
+	
+	@PutMapping("/{bookId}")
+	public ResponseEntity<Book> update(@PathVariable Long bookId, @RequestBody Book book){
+		if(!repository.existsById(bookId)) {
+			return ResponseEntity.notFound().build();
+		}
+		book.setId(bookId);
+		book = repository.save(book);
+		return ResponseEntity.ok(book);
+	}
+	
+	@DeleteMapping("/{bookId}")
+	public ResponseEntity<Void> delete(@PathVariable Long bookId){
+		if(!repository.existsById(bookId)) {
+			return ResponseEntity.notFound().build();
+		}
+		
+		repository.deleteById(bookId);
+		return ResponseEntity.noContent().build();
+	}
+}
